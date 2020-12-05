eef4856788f68a685010b6e8cc0c84476545f097
Benjamin Lindley <benjameslindley@gmail.com>
2020-12-04 18:41:59 -0600

    AddOrEditConference form

--- app/conferences/forms.py
+++ app/conferences/forms.py
@@ -4,6 +4,34 @@ from wtforms.validators import ValidationError, DataRequired, Length
 
 from app.models import Conference
 
+class AddOrEditConferenceForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    hq = StringField('Headquarters')
+    logo = StringField('Logo URL')
+    about = TextAreaField('About')
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_conference=None, *args, **kwargs):
+        super(AddOrEditConferenceForm, self).__init__(*args, **kwargs)
+        self.original_name = None
+        self.original_nickname = None
+        if original_conference:
+            self.original_name = original_conference.name
+            self.original_nickname = original_conference.nickname
+
+    def validate_name(self, name):
+        if (self.original_name is not None) and (name.data != self.original_name):
+            conference = Conference.query.filter_by(name=name.data).first()
+            if conference is not None:
+                raise ValidationError('Please use a different conference name.')
+
+    def validate_nickname(self, nickname):
+        if (self.original_nickname is not None) and (nickname.data != self.original_nickname):
+            conference = Conference.query.filter_by(nickname=nickname.data).first()
+            if conference is not None:
+                raise ValidationError('Please use a different conference nickname.')
+
 class AddConferenceForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     nickname = StringField('Nickname', validators=[DataRequired()])
--- app/conferences/routes.py
+++ app/conferences/routes.py
@@ -1,8 +1,9 @@
-from flask import flash, url_for, redirect, render_template
+from flask import flash, url_for, redirect, render_template, request, abort
+from flask_login import login_required, current_user
 
 from app import db
 from app.models import Conference
-from app.conferences.forms import AddConferenceForm
+from app.conferences.forms import AddConferenceForm, EditConferenceForm, AddOrEditConferenceForm
 from app.conferences import bp
 
 @bp.route('/addconference', methods=['GET', 'POST'])
@@ -27,3 +28,33 @@ def addconference():
 def conferences():
     conferences = Conference.query.all()
     return render_template('conferences/conferences.html', conferences=conferences)
+
+@bp.route('/conferences/<int:id>')
+def conference(id):
+    conference = Conference.query.filter_by(id=id).first_or_404()
+    return render_template('conferences/conference.html', conference=conference)
+
+@bp.route('/edit_conference/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_conference(id):
+    if not current_user.admin:
+        abort(403)
+    conference = Conference.query.filter_by(id=id).first_or_404()
+    form = AddOrEditConferenceForm(original_conference=conference)
+    if form.validate_on_submit():
+        conference.name = form.name.data
+        conference.nickname = form.nickname.data
+        conference.hq = form.hq.data
+        conference.logo = form.logo.data
+        conference.about = form.about.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('conferences.edit_conference', id=id))
+    elif request.method == 'GET':
+        form.name.data = conference.name
+        form.nickname.data = conference.nickname
+        form.hq.data = conference.hq
+        form.logo.data = conference.logo
+        form.about.data = conference.about
+    return render_template('conferences/edit_conference.html', title='Edit Conference',
+                           form=form)
