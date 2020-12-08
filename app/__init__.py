eea4a7839d7a1c59e340385ce6737c5daa353d12
Benjamin Lindley <benjameslindley@gmail.com>
2020-12-08 09:52:21 -0600

    revamping email system

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
-from flask import Flask, request, current_app
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_pretty import Prettify
@@ -17,7 +13,6 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to access this page.'
-mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
 prettify = Prettify()
@@ -30,7 +25,6 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    mail.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
     prettify.init_app(app)
@@ -49,23 +43,6 @@ def create_app(config_class=Config):
 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp)
-
-    if not app.debug and not app.testing:
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'],
-                        app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='NCAAFB Failure',
-                credentials=auth, secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
 
 
     app.jinja_env.globals.update({
