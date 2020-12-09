3160fd92907eb5918a3350ac50e5173185d99fb7
Benjamin Lindley <benjameslindley@gmail.com>
2020-12-09 14:41:28 -0600

    move edit_profile and user to profile bp

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,8 +2,7 @@ import requests
 from flask import Blueprint, render_template, flash, redirect, url_for, request, abort
 from flask_login import current_user, login_required
 from app import db
-from app.main.forms import EditProfileForm
-from app.models import User, Comment
+from app.models import Comment
 
 bp = Blueprint('main', __name__)
 
@@ -22,27 +21,6 @@ def index():
                 break
     return render_template('index.html', title='Home', rankings=rankings, week_number=week_number)
 
-
-@bp.route('/user/<username>')
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
-
-
-@bp.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm(current_user.username)
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.about_me = form.about_me.data
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('main.edit_profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 @bp.route('/delete_comment/<int:id>')
 @login_required
--- app/profile/__init__.py
+++ app/profile/__init__.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from app import db
-from app.profile.forms import LoginForm, RegistrationForm
+from app.profile.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 bp = Blueprint('profile', __name__)
@@ -47,3 +47,23 @@ def register():
                            form=form)
 
 
+@bp.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile/user.html', user=user)
+
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('profile.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('profile/edit_profile.html', title='Edit Profile', form=form)
